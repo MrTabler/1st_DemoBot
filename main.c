cf4c5dd9c31fe44baaf0568df3dddf31628bd57e
MrTabler <61121251+MrTabler@users.noreply.github.com>
2020-02-16 11:44:27 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <kipr/wombat.h>
+
+int main()
+{
+    /*printf("Hello World\n");
+    return 0;*/
+    printTest();
+}
+
+int printTest()
+{
+    printf("Hello World\n");
+    printf("Matt\n");
+    return 0;
+}
+
+
+
